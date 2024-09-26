06d06162289f3dc0fa070337fc384be7d4bb978c
Krish Patel <krishpatel0234@gmail.com>
2024-09-26 13:46:55 +0530

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ export const currency = "₹"
 export const units = "UK"
 // export const backendURL = "http://localhost:8000"
 // export const backendURL = "https://snkr-street-backend.vercel.app"
-export cinst backendURL = "https://snkr-street-backend.onrender.com"
+export const backendURL = "https://snkr-street-backend.onrender.com"
 export const config = {
     headers: {
       "Content-Type": "application/json"
